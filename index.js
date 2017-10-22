07157238522ddc33440d3a3e9470a32a8a633364
Wenjuan Zhang <wenjuanzhang@MacBook-Pro.local>
2017-10-22 15:43:48 -0400

    change https

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ http.createServer(function (req, res) {
         host = domain.split(":")[0];
     ////////////////// change this as per your local setup 
     ////////////////// (or create your own more fancy version! you can use regex, wildcards, whatever...)
-    if (host === "www.vitestore.com") target = {host: "www.vitestore.com", port: "3000"};
-    if (host === "message.vitestore.com") target = {host: "www.vitestore.com", port: "5555"};
+    if (host === "www.vitestore.com") target = {host: "http://www.vitestore.com", port: "3000"};
+    if (host === "message.vitestore.com") target = {host: "https://www.vitestore.com", port: "5555"};
     //////////////////
     proxy.web(req, res, {
         target: target
